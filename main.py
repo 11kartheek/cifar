97b95f8f4e0498e5acffe6adc0527e9d2bb60af6
11kartheek <72185894+11kartheek@users.noreply.github.com>
2024-04-12 20:27:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,142 +1,175 @@
-'''Train CIFAR10 with PyTorch.'''
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
+"""Train CIFAR10 with PyTorch."""
 
+import argparse
+import os
+
+import datasets
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
-import os
-import argparse
-
 from models import *
-from utils import progress_bar
+from utils import transform_test, transform_train
 
-
-# parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-# parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-# parser.add_argument('--resume', '-r', action='store_true',
-#                     help='resume from checkpoint')
+# parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
+# parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
+# parser.add_argument(
+#     "--resume", "-r", action="store_true", help="resume from checkpoint"
+# )
+# parser.add_argument("--num_epochs", "-ne", action="store_true", help="number_of_epochs")
 # args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+class Cifar10SearchDataset(datasets.CIFAR10):
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    def __init__(self, root="~/data", train=True, download=True, transform=None):
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+        super().__init__(root=root, train=train, download=download, transform=transform)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+    def __getitem__(self, index):
+
+        image, label = self.data[index], self.targets[index]
+
+        if self.transform is not None:
+
+            transformed = self.transform(image=image)
+
+            image = transformed["image"]
+
+        return image, label
+
+
+def TestTrainSplit(transform_train, transform_test):
+    trainset = Cifar10SearchDataset(
+        "./data", train=True, download=True, transform=transform_train
+    )
+    testset = Cifar10SearchDataset(
+        "./data", train=False, download=True, transform=transform_test
+    )
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=2
+    )
+
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2
+    )
+    return trainloader, testloader
+
+
+# trainloader, testloader = TestTrainSplit(transform_train, transform_test)
+
+classes = (
+    "plane",
+    "car",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+)
 
 # Model
-print('==> Building model..')
+print("==> Building model..")
 
 net = ResNet18()
 
 net = net.to(device)
-if device == 'cuda':
+if device == "cuda":
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
-# if args.resume:
-#     # Load checkpoint.
-#     print('==> Resuming from checkpoint..')
-#     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-#     checkpoint = torch.load('./checkpoint/ckpt.pth')
-#     net.load_state_dict(checkpoint['net'])
-#     best_acc = checkpoint['acc']
-#     start_epoch = checkpoint['epoch']
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001,
-                      momentum=0.9, weight_decay=5e-4)
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
 
-# Training
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
-    net.train()
-    train_loss = 0
+from tqdm import tqdm
+
+
+def train(model, device, train_loader, optimizer=optimizer, criterion=criterion):
+    model.train()
+    pbar = tqdm(train_loader)
     correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), targets.to(device)
+    processed = 0
+    train_loss = 0
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
+        y_pred = model(data)
+        loss = criterion(y_pred, target)
+        train_loss += loss.item()
         loss.backward()
         optimizer.step()
 
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
+        pred = y_pred.argmax(
+            dim=1, keepdim=True
+        )  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        pbar.set_description(
+            desc=f"Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}"
+        )
+
+    return (train_loss / len(train_loader)), (100 * correct / processed)
 
 
-def test(epoch):
-    global best_acc
-    net.eval()
+def test(model, device, test_loader):
+    model.eval()
     test_loss = 0
     correct = 0
-    total = 0
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(
+                output, target, reduction="sum"
+            ).item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-            test_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
+    test_loss /= len(test_loader.dataset)
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
-    # Save checkpoint.
-    acc = 100.*correct/total
-    if acc > best_acc:
-        print('Saving..')
-        state = {
-            'net': net.state_dict(),
-            'acc': acc,
-            'epoch': epoch,
-        }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.pth')
-        best_acc = acc
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
+        )
+    )
+    return test_loss, 100.0 * correct / len(test_loader.dataset)
 
 
-# for epoch in range(start_epoch, start_epoch+200):
-#     train(epoch)
-#     test(epoch)
-#     scheduler.step()
+def loop(
+    num_epochs,
+    trainloader,
+    testloader,
+    model=net,
+    device=device,
+    optimizer=optimizer,
+    criterion=criterion,
+):
+    for epoch in range(num_epochs):
+        print("EPOCH:", epoch)
+        train(model, device, trainloader, optimizer=optimizer, criterion=criterion)
+        test(model, device, testloader)
+        scheduler.step()
+
+
+# loop(args.num_epochs)
